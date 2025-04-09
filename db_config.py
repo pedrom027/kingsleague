9f146b81f0290fd56401f96a83dc5dd850e6e679
Pedro Mattos <pedrbdo@gmail.com>
2025-04-10 00:36:33 +0100

    Initial commit

--- db_config.py
+++ db_config.py
@@ -1,10 +1,14 @@
+import os
 import MySQLdb
+from urllib.parse import urlparse
 
 def connect_db():
-    return MySQLdb.connect(
-        host="localhost",
-        user="root",
-        passwd="2508",
-        db="kingsleague",
-        charset='utf8mb4'
-    )
+    # Tenta pegar a variável de ambiente JAWSDB_URL (pode ser 'JAWSDB_AQUA_URL' também)
+    db_url = os.environ.get('JAWSDB_URL') or os.environ.get('JAWSDB_AQUA_URL')
+    
+    # Caso não encontre a variável de ambiente, lança um erro
+    if not db_url:
+        raise ValueError("A URL do banco de dados não foi encontrada nas variáveis de ambiente.")
+    
+    # Conectar ao banco de dados usando a URL de conexão
+    return MySQLdb.connect(db_url)
